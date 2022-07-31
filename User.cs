a980256a10184732019211961c0fbd67c926dbca
AdilShahab <adilislamian92@gmail.com>
2022-07-31 19:02:53 +0100

    Add project files.

---
+++ User.cs
@@ -0,0 +1,9 @@
+﻿namespace Dotnet_6_Web_API___Create_JWT_Authentication
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Dotnet_6_Web_API___Create_JWT_Authentication
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
